0c3a84c1129eddd02a53e8ae8ede011091bf422b
yomites <marshall_400@yahoo.com>
2020-05-02 00:01:23 +0300

    Unicafe exercise setup
    Deleted some unnecessary files and edited the index.js file to display the first part of the exercise

---
+++ unicafe/src/index.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setToGood=()=>setGood(good + 1)
+  const setToNeutral=()=>setNeutral(neutral + 1)
+  const setToBad=()=>setBad(bad + 1)
+
+  console.log(good, neutral, bad)
+  
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={setToGood}>
+        good
+      </button>
+      <button onClick={setToNeutral}>
+        neutral
+      </button>
+      <button onClick={setToBad}>
+        bad
+      </button>
+      <h2>statistics</h2>
+      good {good}<br />
+      neutral {neutral}<br />
+      bad {bad}     
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
