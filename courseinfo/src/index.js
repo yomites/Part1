0c3a576b3853cf2985e5081fa441d8d4a1cd3b9b
yomites <marshall_400@yahoo.com>
2020-05-01 09:35:02 +0300

    Update index.js

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -44,8 +44,7 @@ const Total = (props) => {
         Number of exercises {
         props.course.parts[0].exercises + 
         props.course.parts[1].exercises + 
-        props.course.parts[2].exercises
-        }
+        props.course.parts[2].exercises}
       </p>
     </div>
   )
