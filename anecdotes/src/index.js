ac33c3510540f071f9ea8f62c934335d0ca5f47e
yomites <marshall_400@yahoo.com>
2020-05-03 02:42:19 +0300

    Update index.js
    anecdotes step 2.
    A button to vote for the selected anecdote has been added. I tried to implement the app in a way that if more anecdotes are added to the anecdotes array, the new array length will be taken care of automatically.

--- anecdotes/src/index.js
+++ anecdotes/src/index.js
@@ -1,21 +1,40 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const VotesArray=(vote, index)=> {
+  console.log(vote, index)
+  const copy = [ ...vote ]
+  copy[index] += 1
+  return (
+    copy
+  )
+}
+
+const Button=({handleClick, text})=> (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+)
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array.apply
+    (null, new Array(anecdotes.length))
+    .map(Number.prototype.valueOf, 0))
 
-  const setToSelected=()=>
-  setSelected(Math.floor
-    (Math.random() * anecdotes.length) + 0)
-  
+  const setToSelected=()=>setSelected(
+    Math.floor(Math.random() * anecdotes.length) + 0)
   console.log(selected)
+
+  const setToVotes=()=>setVotes(VotesArray(votes, selected))
+  console.log('The value of the array and index', votes, selected)
 
   return (
     <div>
-      {props.anecdotes[selected]} <br />
-      <button onClick={setToSelected}>
-        next anecdote
-      </button>
+      <b>{props.anecdotes[selected]}</b><br />
+      has {votes[selected]} votes <br />
+      <Button handleClick={setToVotes} text="vote" />
+      <Button handleClick={setToSelected} text="next anecdote" />
     </div>
   )
 }
@@ -26,7 +45,10 @@ const anecdotes = [
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  'A primary cause of complexity is that software vendors uncritically adopt almost any feature that users want.',
+  'Prolific programmers contribute to certain disaster.',
+  'Design and programming are human activities; forget that and all is lost.'
 ]
 
 ReactDOM.render(
